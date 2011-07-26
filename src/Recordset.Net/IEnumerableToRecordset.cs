61bba958fd56eb110d9bb2127067ddf2dc564160
Christian Specht <christi@nspecht.de>
2011-07-27 01:05:07 +0200

    initial commit

---
+++ src/Recordset.Net/IEnumerableToRecordset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Recordset.Net
+{
+    public static class IEnumerableToRecordset
+    {
+        public static ADODB.Recordset ToRecordset<T>(this IEnumerable<T> input)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/Recordset.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Recordset.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Recordset.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9b82532-33c3-4d6a-83a8-0e7f934cda1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
