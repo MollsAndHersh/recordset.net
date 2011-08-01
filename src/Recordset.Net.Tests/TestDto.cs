80ac66a508e40ee735b987f7e0439cbc0c90f1ec
Christian Specht <christi@nspecht.de>
2011-08-02 00:05:40 +0200

    add test project and first unit test

---
+++ src/Recordset.Net.Tests/TestDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recordset.Net.Tests
+{
+    public class TestDto
+    {
+        public TestDto(string stringvalue, int int32value, bool boolvalue)
+        {
+            this.StringValue = stringvalue;
+            this.Int32Value = int32value;
+            this.BoolValue = boolvalue;
+        }
+
+        public string StringValue { get; set; }
+        public int Int32Value { get; set; }
+        public bool BoolValue { get; set; }
+    }
+}
---
+++ src/Recordset.Net.Tests/ToRecordsetTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ADODB;
+using NUnit.Framework;
+using Recordset.Net;
+
+namespace Recordset.Net.Tests
+{
+    [TestFixture]
+    public class ToRecordsetTests
+    {
+        List<TestDto> input;
+        ADODB.Recordset rs;
+
+        [SetUp]
+        public void Init()
+        {
+            input = new List<TestDto>();
+            input.Add(new TestDto("string1", 1, true));
+            input.Add(new TestDto("string2", 2, false));
+            rs = input.ToRecordset();
+        }
+
+        [Test]
+        public void Recordset_is_not_null()
+        {
+            Assert.That(rs != null);
+        }
+    }
+}
