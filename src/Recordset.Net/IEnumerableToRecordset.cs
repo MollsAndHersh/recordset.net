820d1e9f4c429f8bc24d11ffa695b0c2a28c17bb
Christian Specht <christi@nspecht.de>
2011-11-02 00:31:34 +0100

    fix XML comments

--- src/Recordset.Net/IEnumerableToRecordset.cs
+++ src/Recordset.Net/IEnumerableToRecordset.cs
@@ -8,10 +8,10 @@ namespace Recordset.Net
     public static class IEnumerableToRecordset
     {
         /// <summary>
-        /// Creates an ADODB.Recordset from an System.Collections.Generic.IEnumerable<T>.
+        /// Creates an ADODB.Recordset from an System.Collections.Generic.IEnumerable(T).
         /// </summary>
         /// <typeparam name="T">The type of the elements of input.</typeparam>
-        /// <param name="input">The System.Collections.Generic.IEnumerable<T> to create an ADODB.Recordset from.</param>
+        /// <param name="input">The System.Collections.Generic.IEnumerable(T) to create an ADODB.Recordset from.</param>
         /// <returns>An ADODB.Recordset that contains elements from the input sequence.</returns>
         public static ADODB.Recordset ToRecordset<T>(this IEnumerable<T> input)
         {
