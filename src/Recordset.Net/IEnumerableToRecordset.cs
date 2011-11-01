6139ef9b10cb9aef693125fcc1e64cd8721f1af8
Christian Specht <christi@nspecht.de>
2011-11-01 19:03:08 +0100

    add XML comments

--- src/Recordset.Net/IEnumerableToRecordset.cs
+++ src/Recordset.Net/IEnumerableToRecordset.cs
@@ -2,8 +2,17 @@
 
 namespace Recordset.Net
 {
+    /// <summary>
+    /// extension methods for IEnumerable
+    /// </summary>
     public static class IEnumerableToRecordset
     {
+        /// <summary>
+        /// Creates an ADODB.Recordset from an System.Collections.Generic.IEnumerable<T>.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of input.</typeparam>
+        /// <param name="input">The System.Collections.Generic.IEnumerable<T> to create an ADODB.Recordset from.</param>
+        /// <returns>An ADODB.Recordset that contains elements from the input sequence.</returns>
         public static ADODB.Recordset ToRecordset<T>(this IEnumerable<T> input)
         {
             return null;
