3d060492b33ecf1f36bee1abdbf7608d2f5b9893
Christian Specht <christi@nspecht.de>
2011-11-02 01:13:27 +0100

    replace NUnit by XUnit.net

--- src/Recordset.Net.Tests/ToRecordsetTests.cs
+++ src/Recordset.Net.Tests/ToRecordsetTests.cs
@@ -1,32 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ADODB;
-using NUnit.Framework;
-using Recordset.Net;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace Recordset.Net.Tests
 {
-    [TestFixture]
     public class ToRecordsetTests
     {
-        List<TestDto> input;
-        ADODB.Recordset rs;
-
-        [SetUp]
-        public void Init()
-        {
-            input = new List<TestDto>();
-            input.Add(new TestDto("string1", 1, true));
-            input.Add(new TestDto("string2", 2, false));
-            rs = input.ToRecordset();
-        }
-
-        [Test]
+        [Fact]
         public void Recordset_is_not_null()
         {
-            Assert.That(rs != null);
+            var input = new List<int>();
+            input.Add(0);
+
+            var rs = input.ToRecordset();
+
+            Assert.NotNull(rs);
         }
     }
 }
