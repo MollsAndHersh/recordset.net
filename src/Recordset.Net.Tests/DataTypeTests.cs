756856645160b8a277b09ee2e8f5f6f74bbdedec
Christian Specht <christi@nspecht.de>
2011-11-07 01:18:45 +0100

    conversion of CLR types to ADO types

---
+++ src/Recordset.Net.Tests/DataTypeTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace RecordsetNet.Tests
+{
+    public class DataTypeTests
+    {
+        [Fact]
+        public void Supported_Type_Is_Converted()
+        {
+            var expected = ADODB.DataTypeEnum.adBoolean;
+
+            var actual = new ADODB.DataTypeEnum();
+            DataTypes.TryGetAdoTypeForClrType(typeof(bool), out actual);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Unsupported_Type_Is_Not_Converted()
+        {
+            bool expected = false;
+
+            var type = new ADODB.DataTypeEnum();
+            bool actual = DataTypes.TryGetAdoTypeForClrType(typeof(object), out type);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Recordset.Net/DataTypes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecordsetNet
+{
+    /// <summary>
+    /// Converts CLR types to ADODB types.
+    /// </summary>
+    internal static class DataTypes
+    {
+        private static readonly Dictionary<Type, ADODB.DataTypeEnum> Types
+            = new Dictionary<Type, ADODB.DataTypeEnum>()
+            {
+                { typeof(Boolean), ADODB.DataTypeEnum.adBoolean },
+                { typeof(Byte), ADODB.DataTypeEnum.adUnsignedTinyInt },
+                { typeof(Char), ADODB.DataTypeEnum.adChar },
+                { typeof(DateTime), ADODB.DataTypeEnum.adDate },
+                { typeof(Decimal), ADODB.DataTypeEnum.adDecimal },
+                { typeof(Double), ADODB.DataTypeEnum.adDouble },
+                { typeof(Guid), ADODB.DataTypeEnum.adVarWChar },
+                { typeof(Int16), ADODB.DataTypeEnum.adSmallInt },
+                { typeof(Int32), ADODB.DataTypeEnum.adInteger },
+                { typeof(Int64), ADODB.DataTypeEnum.adBigInt },
+                { typeof(SByte), ADODB.DataTypeEnum.adTinyInt },
+                { typeof(Single), ADODB.DataTypeEnum.adSingle },
+                { typeof(String), ADODB.DataTypeEnum.adVarWChar },
+                { typeof(UInt16), ADODB.DataTypeEnum.adUnsignedSmallInt },
+                { typeof(UInt32), ADODB.DataTypeEnum.adUnsignedInt },
+                { typeof(UInt64), ADODB.DataTypeEnum.adUnsignedBigInt }
+            };
+
+        /// <summary>
+        /// Get the appropriate ADODB type for a given CLR type.
+        /// </summary>
+        /// <param name="clrType">The CLR type to look for.</param>
+        /// <param name="adoType">The ADODB type that matches the CLR type, if any.</param>
+        /// <returns>True if an ADODB type was found.</returns>
+        public static bool TryGetAdoTypeForClrType(Type clrType, out ADODB.DataTypeEnum adoType)
+        {
+            return Types.TryGetValue(clrType, out adoType);
+        }
+    }
+}
