6e6193ab707db35e32620a90a167e73bb58f68d5
Christian Specht <christi@nspecht.de>
2012-01-14 00:10:20 +0100

    remove duplication in build batch files

--- build-and-run-tests.bat
+++ build-and-run-tests.bat
@@ -1,13 +1,5 @@
 @echo off
 
-rem path to msbuild.exe
-path=%path%;%windir%\Microsoft.net\Framework\v4.0.30319
-
-rem go to current folder
-cd %~dp0
-
-msbuild build.proj /p:RunTests=1
-
-rem To change the output folder, use the following parameter: /p:BuildDir=C:\BuildTest
+build.bat 1
 
 pause
--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ path=%path%;%windir%\Microsoft.net\Framework\v4.0.30319
 rem go to current folder
 cd %~dp0
 
-msbuild build.proj
+msbuild build.proj /p:RunTests="%1"
 
 rem To change the output folder, use the following parameter: /p:BuildDir=C:\BuildTest
 
