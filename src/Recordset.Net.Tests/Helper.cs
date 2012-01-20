404b61c454dcaeba48389904a7a76508685e7e26
Christian Specht <christi@nspecht.de>
2012-01-20 22:01:40 +0100

    add test helper class (finding Recordset fields by name), and tests for it

---
+++ src/Recordset.Net.Tests/Helper.cs
@@ -0,0 +1,19 @@
+﻿
+namespace RecordsetNet.Tests
+{
+    public class Helper
+    {
+        public static bool FieldExistsInRecordset(ADODB.Recordset rs, string fieldName)
+        {
+            foreach (ADODB.Field field in rs.Fields)
+            {                
+                if (field.Name.ToLower() == fieldName.ToLower())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Recordset.Net.Tests/HelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace RecordsetNet.Tests
+{
+    public class HelperTests
+    {
+        public ADODB.Recordset CreateRecordset()
+        {
+            var rs = new ADODB.Recordset();
+            rs.Fields.Append("BoolValue", ADODB.DataTypeEnum.adBoolean);
+            return rs;
+        }
+
+        [Fact]
+        public void FieldExistsInRecordset_ExistingFieldWithExactCase_IsFound()
+        {
+            var rs = this.CreateRecordset();
+            Assert.True(Helper.FieldExistsInRecordset(rs, "BoolValue"));
+        }
+
+        [Fact]
+        public void FieldExistsInRecordset_ExistingFieldWithDifferentCase_IsFound()
+        {
+            var rs = this.CreateRecordset();
+            Assert.True(Helper.FieldExistsInRecordset(rs, "boOlvalUe"));
+        }
+
+        [Fact]
+        public void FieldExistsInRecordset_NotExistingField_IsNotFound()
+        {
+            var rs = this.CreateRecordset();
+            Assert.False(Helper.FieldExistsInRecordset(rs, "foo"));
+        }
+    }
+}
